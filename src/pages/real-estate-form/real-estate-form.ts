57bb9fc702a75497cfda67525a38394606f08b95
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2018-10-21 02:56:08 -0300

    Added incomplete insertion of Real Estate

--- src/pages/real-estate-form/real-estate-form.ts
+++ src/pages/real-estate-form/real-estate-form.ts
@@ -1,3 +1,4 @@
+import { RealEstate } from './../../models/real-estate';
 import { FirebaseProvider } from './../../providers/firebase/firebase';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -8,6 +9,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'real-estate-form.html',
 })
 export class RealEstateFormPage {
+  public realEstate: RealEstate;
 
   constructor(
      public navCtrl: NavController,
@@ -17,6 +19,22 @@ export class RealEstateFormPage {
   }
 
   addRealEstate(){
+    this.realEstate = {
+      name: "Teste", 
+      description: "Teste",
+      zip: "454", 
+      street: "55", 
+      number: "Testeas", 
+      complement: "11212", 
+      district: "3131", 
+      city: "3434", 
+      state: "5345", 
+      type: "644646", 
+      link: "7767", 
+      active: null, 
+      tenantHistory: [], 
+      ownerId: "123" };
+    this.fb.insertData('RealEstate', this.realEstate);
     
   }
 
