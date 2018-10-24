3fc0c73c250fe33e358d2071e644a1211850a414
Felipe Araujo <felipefagyn@gmail.com>
2018-10-24 18:12:06 -0300

    Created contract form layout.

--- src/pages/contract/contract.ts
+++ src/pages/contract/contract.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ModalController
+} from "ionic-angular";
+import { ContractFormPage } from "./contract-form/contract-form";
 
 @IonicPage()
 @Component({
-  selector: 'page-contract',
-  templateUrl: 'contract.html',
+  selector: "page-contract",
+  templateUrl: "contract.html"
 })
 export class ContractPage {
   contracts = null;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    private modalCtrl: ModalController,
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ContractPage');
+    console.log("ionViewDidLoad ContractPage");
   }
 
+  // TODO: verificar se o usuário possui imóveis e inquilinos cadastrados, se sim entra na modal para criar um contrato, senão mostra um alert pedindo que cadastre o que falta.
+  newContract() {
+    let modal = this.modalCtrl.create(ContractFormPage);
+    modal.present();
+  }
 }
