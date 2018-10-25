c8ae1e896c2bb0a812b225dafa95453efb4569fa
Felipe Araujo <felipefagyn@gmail.com>
2018-10-25 16:30:12 -0300

    Updated layout of tenants page

--- src/pages/tenant/tenant.ts
+++ src/pages/tenant/tenant.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ModalController
+} from "ionic-angular";
+import { TenantFormPage } from "./tenant-form/tenant-form";
+import { TenantDetailsPage } from "./tenant-details/tenant-details";
 
 @IonicPage()
 @Component({
-  selector: 'page-tenant',
-  templateUrl: 'tenant.html',
+  selector: "page-tenant",
+  templateUrl: "tenant.html"
 })
 export class TenantPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    private modalCtrl: ModalController,
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TenantPage');
+    console.log("ionViewDidLoad TenantPage");
   }
 
+  newTenant() {
+    let modal = this.modalCtrl.create(TenantFormPage);
+    modal.present();
+  }
+
+  viewDetails(tenant) {
+    let detailsModal = this.modalCtrl.create(TenantDetailsPage, {tenant: tenant});
+    detailsModal.present();
+  }
 }
