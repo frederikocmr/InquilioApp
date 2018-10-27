f9feda98aca35e807950ad18e641499fb8ed92ed
Felipe Araujo <felipefagyn@gmail.com>
2018-10-27 12:29:58 -0300

    Initial layout for tenant details page

--- src/pages/tenant/tenant-details/tenant-details.ts
+++ src/pages/tenant/tenant-details/tenant-details.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the TenantDetailsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-tenant-details',
@@ -21,5 +14,12 @@ export class TenantDetailsPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad TenantDetailsPage');
   }
+  
+  onEditTenant() {
 
+  }
+
+  removeTenant() {
+    
+  }
 }
