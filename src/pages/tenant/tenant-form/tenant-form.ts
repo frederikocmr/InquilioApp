fb2742e9fc1cf5c5315c07c6e691b4683f09434c
Felipe Araujo <felipefagyn@gmail.com>
2018-10-30 16:49:46 -0300

    Changes in form layout and validation.

--- src/pages/tenant/tenant-form/tenant-form.ts
+++ src/pages/tenant/tenant-form/tenant-form.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TenantAccount } from '../../../models/tenant-account';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { regexValidators } from '../../../validators/validator';
 
 @IonicPage()
 @Component({
@@ -10,12 +12,20 @@ import { TenantAccount } from '../../../models/tenant-account';
 export class TenantFormPage {
   editing: Boolean = false;
   tenant: TenantAccount = new TenantAccount();
+  tenantForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) { 
+  constructor(public formBuilder: FormBuilder, public navCtrl: NavController, public navParams: NavParams) { 
     if(this.navParams.get('tenant')){
       this.tenant = navParams.get('tenant');
       this.editing = true;
     }
+    
+    this.tenantForm = this.formBuilder.group({
+      document: ['', Validators.compose([Validators.pattern(regexValidators.cpfCpnj), Validators.required])],
+      name: ['', Validators.required],
+      phone: ['', Validators.compose([Validators.pattern(regexValidators.phone), Validators.required])],
+      email: ['', Validators.compose([Validators.pattern(regexValidators.email), Validators.required])]
+    });
   }
 
   ionViewDidLoad() {
