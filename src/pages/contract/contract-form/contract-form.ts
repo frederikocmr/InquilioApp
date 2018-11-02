96e13585385a2ca260d757a156459eeaf0cf91f7
Felipe Araujo <felipefagyn@gmail.com>
2018-11-02 18:45:18 -0300

    Changed contract form layout and validation

--- src/pages/contract/contract-form/contract-form.ts
+++ src/pages/contract/contract-form/contract-form.ts
@@ -1,12 +1,13 @@
-import { TenantAccount } from './../../../models/tenant-account';
+import { TenantAccount } from "./../../../models/tenant-account";
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { Contract } from "../../../models/contract";
-import { map } from 'rxjs/operators';
+import { map } from "rxjs/operators";
 import { Observable } from "rxjs";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { FirebaseProvider, UiProvider } from "../../../providers";
 import { RealEstate } from "../../../models/real-estate";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @IonicPage()
 @Component({
@@ -15,9 +16,20 @@ import { RealEstate } from "../../../models/real-estate";
 })
 export class ContractFormPage {
   public contract: Contract = new Contract();
+  contractForm: FormGroup;
   public monthShortNames: String[] = [
-    "jan","fev","mar","abr","mai","jun",
-    "jul","ago","set","out","nov","dez"
+    "jan",
+    "fev",
+    "mar",
+    "abr",
+    "mai",
+    "jun",
+    "jul",
+    "ago",
+    "set",
+    "out",
+    "nov",
+    "dez"
   ];
   public realEstates: Observable<RealEstate[]> = null;
   public tenants: Observable<TenantAccount[]> = null;
@@ -25,16 +37,18 @@ export class ContractFormPage {
   public editing: boolean = false;
 
   constructor(
+    public formBuilder: FormBuilder,
     public navCtrl: NavController,
     public navParams: NavParams,
     public ui: UiProvider,
     private fb: FirebaseProvider,
     private afDb: AngularFirestore
   ) {
-    if(this.navParams.get('contract')){
-      this.contract = navParams.get('contract');
+    if (this.navParams.get("contract")) {
+      this.contract = navParams.get("contract");
       this.editing = true;
     }
+
     this.realEstates = this.afDb
       .collection<RealEstate>("RealEstate", ref =>
         ref.where("ownerId", "==", this.fb.user.uid)
@@ -50,18 +64,30 @@ export class ContractFormPage {
           })
         )
       );
+
+    this.contractForm = this.formBuilder.group({
+      beginDate: [this.contract.beginDate ? this.contract.beginDate : "", Validators.required],
+      endDate: [this.contract.endDate ? this.contract.endDate : "", Validators.required],
+      duration: [this.contract.duration ? this.contract.duration : "", Validators.required],
+      realEstateId: [this.contract.realEstateId ? this.contract.realEstateId : "", Validators.required],
+      tenantId: [this.contract.tenantId ? this.contract.tenantId : ""]
+    });
   }
 
   public calculateDuration(): void {
-    if(this.contract.endDate && this.contract.beginDate){ 
-      let timestamp = new Date(this.contract.endDate).getTime() - new Date(this.contract.beginDate).getTime();
-      let seconds = Math.floor((timestamp)/1000);
-      let minutes = Math.floor(seconds/60);
-      let hours = Math.floor(minutes/60);
-      let days = Math.floor(hours/24);
+    let beginDate = this.contractForm.value.beginDate;
+    let endDate = this.contractForm.value.endDate;
+    if (endDate && beginDate) {
+      let timestamp =
+        new Date(endDate).getTime() -
+        new Date(beginDate).getTime();
+      let seconds = Math.floor(timestamp / 1000);
+      let minutes = Math.floor(seconds / 60);
+      let hours = Math.floor(minutes / 60);
+      let days = Math.floor(hours / 24);
       this.contract.duration = this.convertToString(days);
     } else {
-      this.contract.duration = '';
+      this.contract.duration = "";
     }
   }
 
@@ -73,12 +99,19 @@ export class ContractFormPage {
     let year = (days - remainder) / y;
     let month = (remainder - day) / y2;
 
-    var result = 
-    (year > 1 ? year + " anos, " : (year == 1 ?  year + " ano" + (month ? ' , ' : (day ? ' e ' : ' ')) :''))  +
-    (month > 1 ? month + " meses " + (day && year ? 'e ' : (day ? 'e ' : '')) : (month == 1 ?  month + " mês " + (day ? 'e ' : '') : '')) + 
-    (day > 1 ? day + " dias" : (day == 1 ?  day + " dia" :''));
+    var result =
+      (year > 1
+        ? year + " anos, "
+        : year == 1
+          ? year + " ano" + (month ? " , " : day ? " e " : " ")
+          : "") +
+      (month > 1
+        ? month + " meses " + (day && year ? "e " : day ? "e " : "")
+        : month == 1
+          ? month + " mês " + (day ? "e " : "")
+          : "") +
+      (day > 1 ? day + " dias" : day == 1 ? day + " dia" : "");
 
-    
     return result;
   }
 
@@ -89,37 +122,50 @@ export class ContractFormPage {
   public setMinDate(): string {
     return this.today.toISOString();
   }
+	
+	public getValuesFromForm() {
+		let newObject = this.contractForm.value as Contract;
+		this.contract.beginDate = newObject.beginDate;
+		this.contract.endDate = newObject.endDate;
+		this.contract.duration = newObject.duration;
+		this.contract.realEstateId = newObject.realEstateId;
+		this.contract.tenantId = newObject.tenantId;
+	}
 
   public addContract(): void {
     this.ui.showLoading();
-    if(!this.editing){
+    this.getValuesFromForm();
+    if (!this.editing) {
       this.contract.ownerId = this.fb.user.uid;
-      this.fb.insertDataToCollection('Contract', this.contract).then(() => {
-        this.ui.closeLoading();
-        this.ui.showToast(this.fb.message, 2, 'top');
-  
-        if (this.fb.validator) {
-          this.navCtrl.pop();
-        }
-      }).catch((error) => {
-        this.ui.closeLoading();
-        this.ui.showAlert("Erro ao cadastrar", error);
-      });
+      this.fb
+        .insertDataToCollection("Contract", this.contract)
+        .then(() => {
+          this.ui.closeLoading();
+          this.ui.showToast(this.fb.message, 2, "top");
+
+          if (this.fb.validator) {
+            this.navCtrl.pop();
+          }
+        })
+        .catch(error => {
+          this.ui.closeLoading();
+          this.ui.showAlert("Erro ao cadastrar", error);
+        });
     } else {
+      this.fb
+        .updateDataFromCollection("Contract", this.contract)
+        .then(() => {
+          this.ui.closeLoading();
+          this.ui.showToast(this.fb.message, 2, "top");
 
-      this.fb.updateDataFromCollection('Contract', this.contract).then(() => {
-        this.ui.closeLoading();
-        this.ui.showToast(this.fb.message, 2, 'top');
-  
-        if (this.fb.validator) {
-          this.navCtrl.pop();
-        }
-      }).catch((error) => {
-        this.ui.closeLoading();
-        this.ui.showAlert("Erro ao editar", error);
-      });
-
+          if (this.fb.validator) {
+            this.navCtrl.pop();
+          }
+        })
+        .catch(error => {
+          this.ui.closeLoading();
+          this.ui.showAlert("Erro ao editar", error);
+        });
     }
-    
   }
 }
