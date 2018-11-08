27d4b650ff3ad1abfee13232822d0b2560502b96
Felipe Araujo <felipefagyn@gmail.com>
2018-11-08 12:29:03 -0200

    Added selectable options that depend on the stars selected while evaluating the tenant

--- src/pages/tenant/tenant-evaluation/tenant-evaluation.ts
+++ src/pages/tenant/tenant-evaluation/tenant-evaluation.ts
@@ -10,12 +10,14 @@ import { UiProvider, FirebaseProvider } from '../../../providers';
 export class TenantEvaluationPage {
   evaluationForm: FormGroup;
   overallScore: number = 0;
+  scoreItems;
+  scoreQuestion;
   stars = [
-    {value: 1, name: "custom-star-border"},
-    {value: 2, name: "custom-star-border"},
-    {value: 3, name: "custom-star-border"},
-    {value: 4, name: "custom-star-border"},
-    {value: 5, name: "custom-star-border"}
+    { value: 1, name: "custom-star-border" },
+    { value: 2, name: "custom-star-border" },
+    { value: 3, name: "custom-star-border" },
+    { value: 4, name: "custom-star-border" },
+    { value: 5, name: "custom-star-border" }
   ];
 
   constructor(
@@ -23,8 +25,58 @@ export class TenantEvaluationPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public ui: UiProvider,
-    private fb: FirebaseProvider) {    
+    private fb: FirebaseProvider) {
     this.evaluationForm = this.formBuilder.group({});
+  }
+
+  private getScoreItems(score) {
+    switch (score) {
+      case 1:
+        this.scoreItems = [
+          "Nunca pagou em dia",
+          "Não cumpriu integralmente as normas do contrato",
+          "Não conservou o imóvel",
+          "Péssimo relacionamento com os vizinhos"
+        ];
+        this.scoreQuestion = "Nossa, o que aconteceu?";
+        break;
+      case 2:
+        this.scoreItems = [
+          "Atrasou o pagamento (algumas vezes)",
+          "Não cumpriu parte das normas do contrato",
+          "Não se preocupou em conservar o imóvel",
+          "Relacionamento instável com os vizinhos"
+        ];
+        this.scoreQuestion = "Ué, qual foi o problema?";
+        break;
+      case 3:
+        this.scoreItems = [
+          "Não atrasar o pagamento",
+          "Se atentar as normas do contrato",
+          "Conservar o imóvel",
+          "Relacionamento com os vizinhos"
+        ];
+        this.scoreQuestion = "O que poderia ser melhor?";
+        break;
+      case 4:
+        this.scoreItems = [
+          "Pagou em dia",
+          "Cumpriu o contrato",
+          "Conservou bem o imóvel",
+          "Bom relacionamento com os vizinhos"
+        ];
+        this.scoreQuestion = "O que foi bom?";
+        break;
+      case 5:
+        this.scoreItems = [
+          "Pagou sempre em dia ou antecipadamente",
+          "Cumpriu integralmente o contrato",
+          "Se preocupou em manter a conservação do imóvel",
+          "Excelente relacionamento com os vizinhos"
+        ];
+        this.scoreQuestion = "Perfeito, gostaria de destacar algo?";
+        break;
+    }
   }
 
   public getStarName(value): void {
@@ -35,9 +87,10 @@ export class TenantEvaluationPage {
         star.name = "custom-star-border";
       }
     });
-    
+
     this.overallScore = value;
+    this.getScoreItems(value);
   }
 
-  public saveEvaluation(): void {}
+  public saveEvaluation(): void { }
 }
