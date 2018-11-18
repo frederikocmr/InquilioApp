ffe4b6c24a6a80f76d21cd539bb34f19e5127e82
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2018-11-18 20:44:11 -0200

    Added chips values according to score

--- src/pages/tenant/tenant-details/tenant-details.ts
+++ src/pages/tenant/tenant-details/tenant-details.ts
@@ -18,12 +18,7 @@ export class TenantDetailsPage {
   public countScore: number = 0;
   // public isEvaluationTime: boolean = false;
   public isTenantAssociated: boolean = true;
-  public chips = [
-    "Paga sempre em dia",
-    "Cumpre bem o contrato",
-    "Conserva bem o imóvel",
-    "Tem bom relacionamento com os vizinhos"
-  ];
+  public chips = new Array();
 
   constructor(
     private modalCtrl: ModalController,
@@ -39,41 +34,83 @@ export class TenantDetailsPage {
   }
 
   public getScore(): void {
+
     this.afDb.collection<Score>(
       'Score',
       ref => ref.where('tenantId', '==', this.tenant.id)
     ).valueChanges().subscribe(data => {
-      let scoreAux = 0;
+      let scoreAux = 0, carefulScoreAux = 0, contractScoreAux = 0, discretionScoreAux = 0, paymentScoreAux = 0;
+      this.evaluationNumber = 0;
+      this.overallScore = 0;
       data.forEach(item => {
         scoreAux += item.overallScore;
+        carefulScoreAux += (item.carefulScore ? item.carefulScore : 0);
+        contractScoreAux += (item.contractScore ? item.contractScore : 0);
+        discretionScoreAux += (item.discretionScore ? item.discretionScore : 0);
+        paymentScoreAux += (item.paymentScore ? item.paymentScore : 0);
         this.evaluationNumber += 1;
       });
-      this.overallScore =  scoreAux / this.evaluationNumber;
-    }); 
+      this.overallScore = +(scoreAux / this.evaluationNumber).toFixed(2);
+
+      this.getCarefulScore(carefulScoreAux / this.evaluationNumber);
+      this.getContractScore(contractScoreAux / this.evaluationNumber);
+      this.getDiscretionScore(discretionScoreAux / this.evaluationNumber);
+      this.getPaymentScore(paymentScoreAux / this.evaluationNumber);
+
+    });
   }
 
   public getContract(): void {
     this.afDb.collection<Contract>(
       'Contract',
       ref => ref.where('tenantId', '==', this.tenant.id)
-      .where("active", "==", true)
+        .where("active", "==", true)
     ).valueChanges().subscribe(data => {
-      if(data.length > 0 ){
+      if (data.length > 0) {
         this.contract = data[0] as Contract;
-      } 
-    }); 
+      }
+    });
   }
-  
+
   public getDateString(date): string {
     return new Date(date).toLocaleDateString('pt-BR', { timeZone: 'UTC' });
   }
 
   // Shows the tenant evaluation modal (page)
   public evaluateTenant(): void {
-    let modal = this.modalCtrl.create(TenantEvaluationPage, { tenantObj: this.tenant, contractObj: this.contract  });
+    let modal = this.modalCtrl.create(TenantEvaluationPage, { tenantObj: this.tenant, contractObj: this.contract });
     modal.present();
   }
 
   // Makes a tenant disabled (hidden) in the owner's vision
-  public removeTenant(): void {}
+  public removeTenant(): void { }
+
+  public getPaymentScore(score: number) {
+    if ((score >= 4) && (score < 5)) {
+      this.chips.push("Paga em dia");
+    } else if (score >= 5) {
+      this.chips.push("Paga sempre em dia ou antecipadamente");
+    } 
+  }
+  public getContractScore(score) {
+    if ((score >= 4) && (score < 5)) {
+      this.chips.push("Cumpre o contrato");
+    } else if (score >= 5) {
+      this.chips.push("Cumpre integralmente o contrato");
+    } 
+  }
+  public getCarefulScore(score) {
+    if ((score >= 4) && (score < 5)) {
+      this.chips.push("Conserva bem o imóvel");
+    } else if (score >= 5) {
+      this.chips.push("Se preocupa em manter a conservação do imóvel");
+    }
+  }
+  public getDiscretionScore(score) {
+    if ((score >= 4) && (score < 5)) {
+      this.chips.push("Bom relacionamento com os vizinhos");
+    } else if (score >= 5) {
+      this.chips.push("Excelente relacionamento com os vizinhos");
+    } 
+  }
 }
