6b19fb882e63710f093f8cc3a467ab20f5b6e74d
Frederiko Cesar <35873365+frederikocmr@users.noreply.github.com>
2019-08-19 13:06:25 -0300

    Update Index.ts

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -4,10 +4,5 @@ export { ViacepProvider } from './viacep/viacep';
 export { SettingsProvider  } from './settings/settings';
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyBppMFH3ZlpMoDB4RrDjNx94-ZyASl65-M",
-    authDomain: "app-tcc1.firebaseapp.com",
-    databaseURL: "https://app-tcc1.firebaseio.com",
-    projectId: "app-tcc1",
-    storageBucket: "app-tcc1.appspot.com",
-    messagingSenderId: "306866070953"
+  //removed
   };
